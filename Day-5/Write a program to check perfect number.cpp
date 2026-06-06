fcb8ef025d35623c5d880c1a9e54cf4182de17c4
Satyaveer Raj <satyaveerraj454@gmail.com>
2026-06-06 13:23:18 +0530

    Day 5 assignment is uploaded

---
+++ Day-5/Write a program to check perfect number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // add all proper divisors
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == n && n != 0) {
+        cout << n << " is a Perfect Number." << endl;
+    } else {
+        cout << n << " is not a Perfect Number." << endl;
+    }
+
+    return 0;
+}
